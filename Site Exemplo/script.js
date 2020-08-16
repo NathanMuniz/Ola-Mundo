49e6722131e923c555b860cfd6a89014442641a5
Nathan Gabriel Muniz da Silva <natangabriel.palmeiras@gmail.com>
2020-08-16 13:59:44 -0300

    Adicionei Scripit e Imgem

--- Site Exemplo/script.js
+++ Site Exemplo/script.js
@@ -1,1 +1,50 @@
-alert("Olá")
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById("txtano")
+    var res = document.getElementById("res")
+    if (fano.value.length == 0 || fano.value >= ano) {
+        window.alert("[ERRO] Verfique os Dados e tente Novamente")
+    }else{
+        var fsex = document.getElementsByName("radsex")
+        var idade = ano - Number(fano.value)
+        var genero = ""
+        var img = document.createElement("img")
+
+        img.setAttribute("id", "foto")
+        if (fsex[0].checked){
+            genero = "homem"
+            if(idade >= 0 && idade < 10){
+                //Criaça
+                img.setAttribute("src", "foto-bebe-m.png")
+            }else if(idade >= 10 && idade < 21){
+                // Jovem
+                img.setAttribute("src", "foto-jovem-m.png")
+            } else if(idade < 50){
+                // Adulto
+                img.setAttribute("src", "foto-adulto-m.png")
+            }else{
+                //idoso
+                img.setAttribute("src", "foto-idoso-m.png")
+            }
+        }else if (fsex[1].checked){
+            genero = "mulher"
+            if(idade >= 0 && idade < 10){
+                // Crinaça
+                img.setAttribute("src", "foto-bebe-f.png")
+            }else if(idade >= 10 && idade < 21){
+                // Jovem
+                img.setAttribute("src", "foto-jovem-f.png")
+            } else if(idade < 50){
+                // Adulto
+                img.setAttribute("src", "foto-adulto-f.png")
+            }else{
+                //idoso
+                img.setAttribute("src", "foto-idoso-f.png")
+            }
+        }
+        res.style.textAlign = "center"
+        res.innerHTML = `Detetamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+    }
+}
